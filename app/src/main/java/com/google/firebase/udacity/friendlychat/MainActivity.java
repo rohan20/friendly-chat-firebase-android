1f9f19444979490a377e52c5bee4a40dfecf8014
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-12-04 15:14:28 +0530

    added sign out code

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -229,6 +229,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                //sign out
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
